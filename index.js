796730513cbac17c4cf42277c5cac11060fcd738
Denine Guy <denineguy@Denines-MacBook-Pro.local>
2022-07-15 00:23:23 -0400

    the best code solution

--- index.js
+++ index.js
@@ -13,19 +13,20 @@ const {lstat} = fs.promises;
 
 fs.readdir(process.cwd(), async(err, filenames) => {
     if (err) {
-        // error handling code here
         console.log(err)
     }
 
-    for(let filename of filenames) {
-        try {
-            const stats = await lstat(filename);
-            console.log(filename, stats.isFile());
-        } catch(err) {
-            console.log(err);
-        }
-    }
+    const statPromises = filenames.map(filename => {
+        return lstat(filename);
+    })
+    
+    const allStats =  await Promise.all(statPromises);
 
+    for(let stats of allStats) {
+        const index = allStats.indexOf(stats);
+
+        console.log(filenames[index], stats.isFile());
+    }
 })
 
 // Method #1
