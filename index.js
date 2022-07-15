d2e32c0128782ba7373d404b518905e15a7bdbf0
Denine Guy <denineguy@Denines-MacBook-Pro.local>
2022-07-14 23:50:10 -0400

    initial implementation of determining file vs folder

--- index.js
+++ index.js
@@ -13,5 +13,14 @@ fs.readdir(process.cwd(), (err, filenames) => {
         console.log(err)
     }
 
-    console.log(filenames)
+    // BAD CODE HERE!!!
+    for (let filename of filenames) {
+        fs.lstat(filename, (err, stats) => {
+            if(err) {
+                console.log(err);
+            }
+            console.log(filename, stats.isFile());
+        })
+    }
+
 })
