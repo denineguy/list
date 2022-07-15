fba7a21939a9c7de727e0d69b39c1f394c0ff166
Denine Guy <denineguy@Denines-MacBook-Pro.local>
2022-07-14 23:04:53 -0400

    creating a list function

---
+++ index.js
@@ -0,0 +1,15 @@
+//fs is filesystem
+const fs = require('fs')
+
+fs.readdir('.', (err, filenames) => {
+    //Either
+    // err ===  an error object , which means something went wrong 
+    //OR
+    //err === null, which means everything is ok
+    if (err) {
+        // error handling code here
+        console.log(err)
+    }
+
+    console.log(filenames)
+})
